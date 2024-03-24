a7720e04996f794d4d82b7db21a101856f3729d4
amyxcao <124230120+amyxcao@users.noreply.github.com>
2024-03-23 19:15:18 -0700

    web

--- webcam/sketch.js
+++ webcam/sketch.js
@@ -1,20 +1,39 @@
 let mobilenet;
-let puffin;
+let video;
+let label = '';
 
 function modelReady() {
     console.log('Model is ready!!!');
-    mobilenet.predict(puffin, gotResults);
+    mobilenet.predict(gotResults);
 }
 
-function gotResults();
-
-function imageReady() {
-    imageReady(puffin, 0, 0, width, height);
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        label = results[0].className;
+        //let prob = results[0].probability;
+        mobilenet.predict(gotResults);
+    }
 }
+
+// function imageReady() {
+//     imageReady(puffin, 0, 0, width, height);
+// }
 
 function setup() {
     createCanvas(640, 480);
+    video = createCapture(VIDEO);
+    //puffin = createImg('puffin.jpg');
+    video.hide();
     background(0);
-    puffin = createImg('puffin.jpg');
-    mobilenet = ml5.imageClassifier('MobileNet', modelReady);
+    mobilenet = ml5.imageClassifier('MobileNet', video, modelReady);
+}
+
+function draw() {
+    image(video, 0, 0);
+    fill(0);
+    textSize(32);
+    text(label, 10, height - 100);
 }
